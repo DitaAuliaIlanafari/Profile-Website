590d90cccb9b93401811985feb62104d234e991f
DitaAuliaIlanafari <ditailanafari@gmail.com>
2024-12-12 17:23:56 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Skills from "./components/Skills";
 import Testimoni from "./components/Testimoni";
 import About from "./components/Aboutme";
 import Portofolio from "./components/Portofolio";
-import Hero from "./components/Hero";
+import Hero from "./components/hero";
 import Contact from "./components/Contact";
 
 function App() {
